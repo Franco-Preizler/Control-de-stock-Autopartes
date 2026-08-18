fb18499c89920c228ee343e8f04cf6b37a306a2b
eibanez@uade.edu.ar <Emaa1910>
2026-08-18 17:12:08 -0300

    En proceso

--- Lista/Validacion_Stock.py
+++ Lista/Validacion_Stock.py
@@ -1,7 +1,7 @@
-def validarStock(a,b):
+def validarStock(a):
     print(a)
     prod=input("Elija un producto para agregar stock")
-    while prod not in b:
+    while prod not in a:
         print("El producto elegido es incorrecto")
         prod=input("Elija un producto para agregar stock:  ")
     return prod
@@ -11,6 +11,18 @@ suspe=["amortiguador","espiral","buje","rotula","bieleta"]
 mante=["ruleman","tensor","filtro de combustible","filtro de aire","filtro de aceite"]
 mot=["carter","piston","biela","valvula","cigueñal"]
 carro=["paragolpe","optica","capot","manija","espejo"]
+
+if opcion ==1:
+    producto=validarStock(suspe)
+elif opcion == 2:
+    producto=validarStock(mante)
+elif opcion == 3:
+    producto=validarStock(mot)
+elif opcion == 4: 
+    producto=validarStock(carro)
+
+
+
 
 
 
