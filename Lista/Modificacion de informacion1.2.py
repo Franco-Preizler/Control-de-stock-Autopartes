3feb9537cd49d744792a479de8763f5e674b3171
Emaa1910 <eibanez@uade.edu.ar>
2026-08-25 17:12:38 -0300

    Update Modificacion de informacion1.2.py

--- Lista/Modificacion de informacion1.2.py
+++ Lista/Modificacion de informacion1.2.py
@@ -12,30 +12,7 @@ def modificar_info(modi):
         modificado=input("Ingrese informacion nueva: ")
         modi[2]=modificado
 
-opcion_gestion=2
-motor = [["Motor", 101,"Filtro de aceite", 0],
-        ["Motor", 102 ,"Correa de distribución", 0],
-        ["Motor", 103 ,"Bomba de aceite", 0],
-        ["Motor", 104,"Junta de culata", 0],
-        ["Motor", 105,"Pistón", 0]]
 
-encendido = [["Encendido", 201,"Bujía", 0],
-    ["Encendido", 202,"Bobina de encendido", 0],
-    ["Encendido", 203, "Cables de bujía", 0],
-    ["Encendido", 204, "Distribuidor", 0],
-    ["Encendido", 205, "Motor de arranque", 0]]
-
-refrigeracion = [["Refrigeración", 301, "Radiador", 0],
-        ["Refrigeración", 302,  "Bomba de agua", 0],
-        ["Refrigeración", 303, "Termostato", 0],
-        ["Refrigeración", 304, "Electroventilador", 0],
-        ["Refrigeración", 305, "Manguera de radiador", 0]]
-
-suspension = [["Suspensión", 401, "Amortiguador delantero", 0],
-        ["Suspensión", 402, "Amortiguador trasero", 0],
-        ["Suspensión", 403, "Rótula", 0],
-        ["Suspensión", 404, "Bieleta", 0],
-        ["Suspensión", 405, "Bujes de suspensión", 0]]
 if opcion_gestion == 2:
     codigo=int(input("Ingresar codigo de producto: "))
     while codigo<100 or codigo >500:
