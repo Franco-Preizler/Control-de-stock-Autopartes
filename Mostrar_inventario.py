cb4b98e22d1d7842b9b57db7fcd386da6fb7585c
Franco-Preizler <fpreizler@uade.edu.ar>
2026-09-01 15:00:45 -0300

    Add files via upload
    Corrección

---
+++ Mostrar_inventario.py
@@ -0,0 +1,25 @@
+def mostrar_autopartes(motor, encendido, refrigeracion, suspension):
+
+    print("\n========== INVENTARIO COMPLETO ==========")
+
+    matrices = [
+        motor,
+        encendido,
+        refrigeracion,
+        suspension
+    ]
+
+    for matriz in matrices:
+
+        for autoparte in matriz:
+
+            print(
+                "Categoría:", autoparte[0],
+                "| Código:", autoparte[1],
+                "| Autoparte:", autoparte[2],
+                "| Marca:", autoparte[3],
+                "| Modelo:", autoparte[4],
+                "| Stock:", autoparte[5],
+                "| Precio:", autoparte[6],
+                "| Stock mínimo:", autoparte[7]
+            )
