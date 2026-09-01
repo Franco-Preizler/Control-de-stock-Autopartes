4bdc686387ca01ca948876ea0cbd0ef92231d97b
Emaa1910 <eibanez@uade.edu.ar>
2026-09-01 15:08:23 -0300

    Update mainmenu.py

--- mainmenu.py
+++ mainmenu.py
@@ -3,6 +3,38 @@ from Modificar_stock import modificar_autoparte
 from eliminarap import eliminar_autoparte
 from Consulta_total import mostrar_autopartes
 
+motor = [
+    ["Motor", 101, "Filtro de aceite", "BGK", "Volkswagen Polo", 6, 71000, 5],
+    ["Motor", 102, "Correa de distribución", "BGK", "Peugeot 208", 7, 40000, 5],
+    ["Motor", 103, "Bomba de aceite", "BGK", "Volkswagen Polo", 9, 43000, 5],
+    ["Motor", 104, "Junta de culata", "BGK", "Volkswagen Polo", 11, 59000, 5],
+    ["Motor", 105, "Pistón", "BGK", "Volkswagen Polo", 17, 56000, 5]
+]
+
+encendido = [
+    ["Encendido", 201, "Bujía", "NGK", "Universal", 19, 18900, 5],
+    ["Encendido", 202, "Bobina de encendido", "Bosch", "Universal", 25, 34800, 5],
+    ["Encendido", 203, "Cables de bujía", "Bosch", "Universal", 30, 23600, 5],
+    ["Encendido", 204, "Distribuidor", "Bosch", "Universal", 13, 23000, 5],
+    ["Encendido", 205, "Motor de arranque", "Bosch", "Universal", 9, 32000, 5]
+]
+
+refrigeracion = [
+    ["Refrigeración", 301, "Radiador", "Valeo", "Universal", 17, 15900, 5],
+    ["Refrigeración", 302, "Bomba de agua", "SKF", "Universal", 24, 1090, 5],
+    ["Refrigeración", 303, "Termostato", "Gates", "Universal", 70, 9500, 5],
+    ["Refrigeración", 304, "Electroventilador", "Valeo", "Universal", 45, 9200, 5],
+    ["Refrigeración", 305, "Manguera de radiador", "Gates", "Universal", 80, 8900, 5]
+]
+
+suspension = [
+    ["Suspensión", 401, "Amortiguador delantero", "Monroe", "Universal", 22, 4500, 5],
+    ["Suspensión", 402, "Amortiguador trasero", "Monroe", "Universal", 25, 4800, 5],
+    ["Suspensión", 403, "Rótula", "TRW", "Universal", 28, 5800, 5],
+    ["Suspensión", 404, "Bieleta", "TRW", "Universal", 9, 5700, 5],
+    ["Suspensión", 405, "Bujes de suspensión", "TRW", "Universal", 24, 5000, 5]
+]
+
 print("⏤"*120)
 print(" "*45,"BIENVENIDO AL SISTEMA DE CONTROL DE STOCK DE AUTOPARTES PARTSCTRL\n")
 print("⏤"*120)
@@ -25,13 +57,34 @@ print(""" \nSeleccione las siguientes opciones para comenzar con la carga del si
         if option == "0":
             print("\n > Gracias por utilizar el sistema de control de stock de autopartes")
         elif option == "1":
-            agregar_autoparte()
+            agregar_autoparte(
+            motor,
+            encendido,
+            refrigeracion,
+            suspension
+        )
+
         elif option == "2":
-            modificar_autoparte()
+            modificar_autoparte(
+            motor,
+            encendido,
+            refrigeracion,
+            suspension
+        )
         elif option == "3":
-            eliminar_autoparte()
+            eliminar_autoparte(
+            motor,
+            encendido,
+            refrigeracion,
+            suspension
+        )
         elif option == "4":
-            mostrar_autopartes()
+            mostrar_autopartes(
+            motor,
+            encendido,
+            refrigeracion,
+            suspension
+        )
         elif option == "5":
             consultar_autoparte()
         elif option == "6":
